ec7a0cfac0af76cb401f48f7022b0cedd51df85c
imadeddine2005 <oukrati.imadeddine@ensam-casa.ma>
2026-03-27 19:23:18 +0100

    first commit - SmartRecruite App

---
+++ Client/src/App.jsx
@@ -0,0 +1,30 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+
+function App() {
+
+  return (
+    <>
+    
+    <Router>
+      <div className='container'>
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </div>
+    </Router>
+    <ToastContainer /> 
+    </>
+  )
+}
+
+export default App
---
+++ Client/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom"
+import {FaSignInAlt , FaSignOutAlt , FaUser} from 'react-icons/fa'
+import { useSelector , useDispatch} from "react-redux"
+import { logout, reset } from "../features/auth/authSlice"
+import { useNavigate } from "react-router-dom"
+
+function Header() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+
+  const handleLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/login')
+  }
+
+  return (
+    <header className="header">
+        <div className="logo">
+            <Link to="/">GoalSeter</Link>
+        </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className="btn" onClick={handleLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li><Link to="/login"><FaSignInAlt/> Login    </Link></li>
+            <li><Link to="/register"><FaUser/> Register</Link></li>
+          </>
+        )}
+      </ul>
+    </header>
+  )
+}
+export default Header
